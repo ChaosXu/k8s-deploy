2bad960c8089b8ed9413ec4c9f999080b25207c7
ChaosXu <jun.chaos@gmail.com>
2018-12-15 12:01:31 +0800

    fix

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-source ~/.profile
+# setup proxy for shell
+export HTTP_PROXY=http://10.0.2.2:8118
+export HTTPS_PROXY=${HTTP_PROXY}
+export NO_PROXY=localhost,127.0.0.1,kubernetes
 
-# setup centos7
+# swap off
+swapoff -a 
+# disable firewalld
+systemctl disable firewalld && systemctl stop firewalld
 # Set SELinux in permissive mode (effectively disabling it)
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
@@ -14,12 +20,19 @@ EOF
 sysctl --system
 
 # install docker
-yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
 docker_version=18.06.1.ce-3.el7
 
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce-${docker_version} -y
+
+# setup docker proxy
+mkdir -p /etc/systemd/system/docker.service.d
+cat <<EOF > /etc/systemd/system/docker.service.d/http-proxy.conf
+[Service]
+Environment="HTTP_PROXY=${HTTP_PROXY}" "HTTPS_PROXY=${HTTP_PROXY} "NO_PROXY=${NO_PROXY}"
+EOF
+systemctl daemon-reload &&\
 systemctl enable docker && systemctl start docker
 
 # install kubeadm
